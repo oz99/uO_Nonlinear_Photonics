90ebe5df8982356578cf465e81670cacd4f0828c
Waqaar Hussain <133903574+waqaarwh7@users.noreply.github.com>
2023-05-26 16:58:38 -0400

    waqaar

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
-print('Hello World')
+import meep as mp
+cell = mp.Vector3(16,8,0)
+geometry = [mp.Block(mp.Vector3(mp.inf,1,mp.inf),
+                     center=mp.Vector3(),
+                     material=mp.Medium(epsilon=12))]
+sources = [mp.Source(mp.ContinuousSource(frequency=0.15),
+                     component=mp.Ez,
+                     center=mp.Vector3(-7,0))]
+pml_layers = [mp.PML(1.0)]
+resolution = 10
+sim = mp.Simulation(cell_size=cell,
+                    boundary_layers=pml_layers,
+                    geometry=geometry,
+                    sources=sources,
+                    resolution=resolution)
+sim.run(until=200)
+import numpy as np
+import matplotlib.pyplot as plt
 
-#Hello Can you read this
+eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+plt.figure()
+plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+plt.axis('off')
+plt.show()
 
-# Hello Waqaaar How are you?
-
-#Test2
+ez_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+plt.figure()
+plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu', alpha=0.9)
+plt.axis('off')
+plt.show()
