15d677442aad3c22a7feb019c4b58deb13dc2351
oz99 <ooner083@uottawa.ca>
2024-05-23 21:45:38 -0400

    Updated Graphs for SPM ploting

--- Experimental_Setups/Methods/Plotting_Codes/SPM_plot_NA_all_devices_nomalized_2.5D.py
+++ Experimental_Setups/Methods/Plotting_Codes/SPM_plot_NA_all_devices_nomalized_2.5D.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import glob
 import os
 import numpy as np  # for logarithm
 import re
+
 
 def mw_to_dbm(power_mw):
     return 10 * np.log10(power_mw)
@@ -15,13 +18,19 @@ def generate_offset_plots_for_wavelength_range(all_file_names, data_directory, s
     for wavelength in wavelengths:
         filtered_files = [fn for fn in all_file_names if f"-{wavelength}-" in fn]
 
-        plt.figure(figsize=(12, 8))
+        plt.figure(figsize=(12, 10))
         offset = 0  # Initialize offset for the highest power plot
 
         # Sort files in ascending order of power
         sorted_files = sorted(filtered_files, key=lambda fn: float(fn.split('-')[-2].replace('mW', '')))
 
-        for file_name in sorted_files[::-1]:  # Reverse the list to start from the highest power
+        # Create a colormap
+        cmap = cm.plasma
+        num_lines = len(sorted_files)
+        colors = cmap(np.linspace(0.1, 0.9, num_lines))
+
+        # Loop through files and plot
+        for counter, file_name in enumerate(sorted_files[::-1]):  # Reverse the list to start from the highest power
             input_power_mw = float(file_name.split('-')[-2].replace('mW', ''))
             input_power_dbm = mw_to_dbm(input_power_mw)
             data = pd.read_csv(file_name, delimiter='\t', header=None, names=['X', 'Y'])
@@ -30,12 +39,42 @@ def generate_offset_plots_for_wavelength_range(all_file_names, data_directory, s
             max_y_dbm = mw_to_dbm(max_y_value)  # Convert peak power to dBm
             normalized_y = (data['Y'] / max_y_value if max_y_value != 0 else data['Y']) + offset
 
-            plt.plot(data['X'], normalized_y, label=f'P_in={input_power_dbm:.2f}dBm')
-            
-            #plt.plot(data['X'], normalized_y, label=f'Pin={input_power_dbm:.2f}dBm Pout={max_y_dbm:.2f}dBm')
+            # Plot the data with color from the gist_heat colormap
+            plt.plot(data['X'], normalized_y, label=f'{input_power_dbm:.2f}', color=colors[counter],linewidth=2)
 
+            mpl.rcParams['axes.linewidth'] = 1
+            plt.tick_params(width=2)
+            plt.xlim(min(data['X']), max(data['X'])-4)
+            plt.ylim(-2.7, 1.1)
+            plt.gca().set_yticks([])
+            plt.gca().set_yticklabels([])
             # Decrement the offset for the next plot so lower powers are below
             offset -= offset_step
+
+        # for file_name in sorted_files[::-1]:  # Reverse the list to start from the highest power
+        #     input_power_mw = float(file_name.split('-')[-2].replace('mW', ''))
+        #     input_power_dbm = mw_to_dbm(input_power_mw)
+        #     data = pd.read_csv(file_name, delimiter='\t', header=None, names=['X', 'Y'])
+
+        #     max_y_value = data['Y'].max()
+        #     max_y_dbm = mw_to_dbm(max_y_value)  # Convert peak power to dBm
+        #     normalized_y = (data['Y'] / max_y_value if max_y_value != 0 else data['Y']) + offset
+
+
+        #     cmap = cm.gist_heat
+        #     num_lines = len(sorted_files)
+        #     colors = cmap(np.linspace(0, 1, num_lines))
+        #     counter = 1
+
+        #     plt.plot(data['X'], normalized_y, label=f'{input_power_dbm:.2f}dBm', color=colors[counter])
+        #     plt.gca().set_yticklabels([])
+        #     counter += 1
+        #     # plt.set(ylabel=None)  # remove the y-axis label
+            
+        #     #plt.plot(data['X'], normalized_y, label=f'Pin={input_power_dbm:.2f}dBm Pout={max_y_dbm:.2f}dBm')
+
+        #     # Decrement the offset for the next plot so lower powers are below
+        #     offset -= offset_step
 
         ## Each device has a specific length. I want to make a list for the lengths of each device and reutn the value to device_length
         if device == 'L00-D0':
@@ -66,10 +105,14 @@ def generate_offset_plots_for_wavelength_range(all_file_names, data_directory, s
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=20)
         #plt.title(f'SPM at {wavelength} for Hugyens MetaWaveguide L={device_length}um',fontsize=24)
-        plt.legend(loc='upper right',fontsize=16)
+        plt.legend(title='$\mathregular{P_{in-avg}}$ (dBm)', title_fontsize = 20, loc='lower left',
+                    fontsize=20,handleheight=2.22, bbox_to_anchor=(0, 0.0075), framealpha=1,
+                    edgecolor='black', frameon = True, fancybox=True ) #loc='upper right' loc='lower left',
+        
+        #plt.subplots_adjust(bottom=-0.05)
         s = wavelength
         number = int(re.search(r'\d+', s).group())
-        plt.text(number-20, 0.8, f'Huygens L={device_length}um', fontsize=20)
+        plt.text(number-19.5, 0.8, f'Huygens L={device_length}um', fontsize=24)
 
         device_folder = os.path.join(data_directory, device)
         if not os.path.exists(device_folder):
